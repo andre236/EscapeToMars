b7ac27446f712b95ba17fb12c7f7dfdb32eaec79
Ancarus <adr_hb@hotmail.com>
2021-06-26 01:39:30 -0300

    Add skin Mega A. w/ chroma Red Blue; Changing Script Skin costumer; Config Animations and tests

--- EscapeToMars/Assets/Scritps/SkinManager.cs
+++ EscapeToMars/Assets/Scritps/SkinManager.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class SkinManager : MonoBehaviour
 {
+    [SerializeField]
+    private int _state, _priceSkin, _indexSkin;
 
+    [SerializeField]
+    private Text _equipTXT;
+    private Button _equipButton;
+    
     public GameObject[] SkinsPrefabs;
-
     public static SkinManager instance;
-
-
+    
     void Awake()
     {
        if(instance == null)
@@ -25,18 +26,55 @@ public class SkinManager : MonoBehaviour
         }
     }
 
+    public void ControlEquip(int state, int priceSkin, int indexSkin)
+    {
+        switch (state)
+        {
+            case 0:
+                _equipTXT.text = "Unavaliable";
+                _equipButton.interactable = false;
+                break;
+            case 1:
+                _equipTXT.text = "Redeem Skin";
+                _equipButton.interactable = true;
+                RedeemSkin(priceSkin, indexSkin);
+                break;
+            case 2:
+
+                break;
+            case 3:
+                break;
+        }
+    }
+
+    public void RedeemSkin(int numberStars, int indexSkin)
+    {
+        Color redColor = new Color(173, 23, 44);
+       
+        if (numberStars <= StarsCollection.instance.TotalStars && PlayerPrefs.GetInt("HaveSkin"+indexSkin) == 0)
+        {
+            _equipTXT.color = Color.white;
+            PlayerPrefs.SetInt("HaveSkin" + indexSkin.ToString(), 1);
+            _equipTXT.text = "Equip";
+        }
+        else
+        {
+            _equipTXT.color = redColor;
+        }
+    }
+
 
     public void EquipCustom(int indexSkin)
     {
-        PlayerPrefs.SetInt("IndexSkin", indexSkin);
-        Debug.Log("Skin: " + SkinsPrefabs[PlayerPrefs.GetInt("IndexSkin")].name + " equipada com sucesso!");
-    }
-
-    public void RedeemSkin(int numberStars)
-    {
-        if(numberStars >= StarsCollection.instance.TotalStars)
+        if(PlayerPrefs.GetInt("HaveSkin"+indexSkin) == 1 && PlayerPrefs.GetInt("IndexSkin") != indexSkin)
         {
-            
+            PlayerPrefs.SetInt("IndexSkin", indexSkin);
+            _equipTXT.text = "Equipped";
+            Debug.Log("Skin: " + SkinsPrefabs[PlayerPrefs.GetInt("IndexSkin")].name + " equipada com sucesso!");
+        }
+        else
+        {
+            Debug.Log("A skin: " + SkinsPrefabs[PlayerPrefs.GetInt("IndexSkin")].name + " já está equipada!");
         }
     }
 
